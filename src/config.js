c59e763c5cb07f8fcc5edff5c040067b37f25cde
Eugene Levit <eug.levit@gmail.com>
2018-02-04 14:12:40 -0500

    heroku deploy

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080';
+export const API_BASE_URL = 'https://salty-falls-95504.herokuapp.com' || 'http://localhost:8080';
 
 export const CLIENT_ORIGIN = 'https://salty-falls-95504.herokuapp.com' || 'http://localhost:8080';
