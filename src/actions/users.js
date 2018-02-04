b0e20eca71d8aec86693c47fb661a4b3d22d4d09
Eugene Levit <eug.levit@gmail.com>
2018-02-04 14:04:47 -0500

    heroku deploy

--- src/actions/users.js
+++ src/actions/users.js
@@ -1,10 +1,7 @@
-// import {SubmissionError} from 'redux-form';
-
 import {normalizeResponseErrors} from './utils';
-// import {CLIENT_ORIGIN} from '../config';
+import {API_BASE_URL} from '../config';
 
 
-const API_BASE_URL = process.env.CLIENT_ORIGIN || 'http://localhost:8080';
 
 export const registerUser = user => dispatch => {
     return fetch(`${API_BASE_URL}/api/users`, {
